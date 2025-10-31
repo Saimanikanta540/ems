2bd37b910b79f30a24dc34713af19df0b30328f6
PavanKarthikGaraga <pavankarthik107@gmail.com>
2025-11-01 00:53:29 +0530

    changed server port to 8080

--- backend/src/main/java/com/acme/app/AcmeApiApplication.java
+++ backend/src/main/java/com/acme/app/AcmeApiApplication.java
@@ -2,10 +2,16 @@ package com.acme.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AcmeApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AcmeApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AcmeApiApplication.class, args);
