216544921661a40a29c5a29aaf5b8a924c10ef5b
PavanKarthikGaraga <pavankarthik107@gmail.com>
2025-11-01 00:49:48 +0530

    added health endpoint to application.properties

--- backend/src/main/java/com/acme/app/AcmeApiApplication.java
+++ backend/src/main/java/com/acme/app/AcmeApiApplication.java
@@ -2,12 +2,12 @@ package com.acme.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AcmeApiApplication {
+public class AcmeApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AcmeApiApplication.class, args);
     }
-
 }
